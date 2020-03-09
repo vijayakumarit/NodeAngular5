07c02520a5c445f8dfc40eb2b45c2eb34d507b5b
OEM Configuration (temporary user) <vijaykumar@torvistech.com>
2020-03-09 16:51:30 +0530

    db changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
+  database: 'mongodb://admin:arielWINTE6@ds161062.mlab.com:61062/mydb',   //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
